77d5b512e8d5baffadbf1473fe7ae1b7a98187d4
onixChiconela <you@example.com>
2024-06-24 13:09:40 +0200

    project creation

---
+++ src/test/java/trafficmanagement/traffic/TrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package trafficmanagement.traffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
